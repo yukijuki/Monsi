58ee4e8c8f7b5c745b565cfac98e5848a1af4c08
Yuki Kawano <yukijukisfc@gmail.com>
2019-11-28 17:12:12 +0900

    backend done

--- app.py
+++ app.py
@@ -1,14 +1,12 @@
 from flask import Flask
-from flask import request, redirect, render_template, url_for
+from flask import request, redirect, send_from_directory, jsonify
 from flask_sqlalchemy import SQLAlchemy
-from flask_security import Security, SQLAlchemyUserDatastore, UserMixin, RoleMixin, login_required
 
 
 app = Flask(__name__)
+
 app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://postgres:fmg9akimbo@localhost/monsi"
 app.config["SECRET_KEY"] = "super-secret"
-app.config["SECURITY_REGISTERABLE"] = True
-app.config['SECURITY_PASSWORD_HASH'] = "plaintext"
 
 app.debug = True
 db = SQLAlchemy(app)
@@ -16,52 +14,48 @@ db = SQLAlchemy(app)
 
 # Define Models
 
-roles_users = db.Table("roles_users", 
-    db.Column("user_id", db.Integer(), db.ForeignKey("user.id")),
-    db.Column("role_id", db.Integer(), db.ForeignKey("role.id"))
-)
-
-class Role(db.Model, RoleMixin):
-    id = db.Column(db.Integer(), primary_key=True)
-    name = db.Column(db.String(80), unique=True)
-    description = db.Column(db.String(255))
-
-class User(db.Model, UserMixin):
+class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(255), unique=True)
     password = db.Column(db.String(255))
-    active = db.Column(db.Boolean())
     confirmed_at = db.Column(db.DateTime())
-    roles = db.relationship("Role", secondary=roles_users, 
-                            backref=db.backref("users", lazy=("dynamic")))
+    
 
-# Setup Flask-Security
-user_datastore = SQLAlchemyUserDatastore(db, User, Role)
-security = Security(app, user_datastore)
+class Image(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    url = db.Column(db.String(255), unique=True)
+    link = db.Column(db.String(255), unique=True)
+    count = db.Column(db.Integer)
 
-#USer test
-# @app.before_first_request
-# def create_user():
-#     db.create_all()
-#     user_datastore.create_user(email="yukijukisfc@gmail.com", password="test123")
-#     db.session.commit()
+@app.route("/register", methods=["POST"])
+def register():
+    data = request.get_json()
 
-@app.route("/")
-def index():
-    return render_template("add_user.html")
-
-@app.route("/profile/<email>")
-@login_required
-def profile(email):
-    user = User.query.filter_by(email=email).first()
-    return render_template("profile.html", user=user)
-
-@app.route("/post_user", methods=["POST"])
-def post_user():
-    user = User(request.form["username"], request.form["email"])
+    user = User(data["email"], data["password"])
     db.session.add(user)
     db.session.commit()
-    return redirect(url_for("index"))
+
+    return 200
+
+@app.route("/login", methods=["GET"])
+def login():
+    data = request.get_json()
+
+    user_data = User.query.filter_by(email=data["email"]).first()
+    if user_data is None:
+        return 404
+    elif user_data["password"] == data["password"]:
+        return 200
+    elif user_data["password"] == "":
+        return 403
+    else:
+        return 418
+
+
+@app.route("/get_images", methods=["Get"])
+def get_images():
+    data = Image.query.all()
+    return jsonify({"url":data["url"], "link":data["link"]})
 
 if __name__ == "__main__":
     app.run()
